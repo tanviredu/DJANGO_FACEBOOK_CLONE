598df55f548faa474f0944bae95fa545db6d126c
tanviredu <tanviredu2018@gmail.com>
2024-04-22 13:11:57 +0600

    added

--- facebook_prj/facebook_prj/settings.py
+++ facebook_prj/facebook_prj/settings.py
@@ -1,10 +1,12 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
+STATIC_DIR = os.path.join(BASE_DIR,"static")
+MEDIA_DIR = os.path.join(BASE_DIR,"media")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -20,6 +22,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom django admin
+    "jazzmin",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'facebook_prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +123,81 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(STATIC_URL, 'staticfiles') # no need to ceeate dir
+
+STATICFILES_DIRS = STATIC_DIR,
+MEDIA_URL = "/media/"
+MEDIA_ROOT = MEDIA_DIR
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    'site_header': "EDU Student Portal",
+    'site_brand': "Connecting Edu Engineers",
+    'site_logo': "images/logo.png",
+    'copyright':  "All Right Reserved 2023",
+    "welcome_sign": "Welcome EDU Portal",
+    "topmenu_links": [
+    ],
+
+    "order_with_respect_to": [
+        "core",
+        "userauth",
+        "addon",
+    ],
+    
+    "icons": {
+        "admin.LogEntry": "fas fa-file",
+
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+
+        "userauths.User": "fas fa-user",
+        "userauths.Profile":"fas fa-address-card",
+
+        "core.post": "fas fa-th",
+        "core.Page": "fas fa-users",
+        "core.ReplyComment": "fas fa-reply",
+        "core.group": "fas fa-layer-group",
+        "core.notification": "fas fa-bell",
+        "core.Comment":"fas fa-comments",
+        "core.Friend":"fas fa-users",
+        "core.FriendRequest":"fas fa-user-plus",
+    },
+
+    "show_ui_builder" : True
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": "navbar-indigo",
+    "accent": "accent-olive",
+    "navbar": "navbar-indigo navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-indigo",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
